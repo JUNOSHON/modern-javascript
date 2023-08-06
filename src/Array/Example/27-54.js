dde73bdc3f37c5c12666524f901a3b4ee66a968b
junoshon <sanoldfox@naver.com>
2023-08-06 11:29:31 +0900

    concat, shift, unshift

---
+++ src/Array/Example/27-54.js
@@ -0,0 +1,5 @@
+const arr = [1,2];
+
+let result = arr.shift();
+console.log(result);
+console.log(arr);
---
+++ src/Array/Example/27-59.js
@@ -0,0 +1,7 @@
+const arr = [3,4];
+arr.unshift([1,2])
+arr.push([5,6])
+console.log(arr);
+
+let result = [1,2].concat([3,4])
+console.log(result);
