183ffcd601df1d5bca050f2560995b4c03a182b4
junoshon <sanoldfox@naver.com>
2023-09-22 16:27:40 +0900

    typecasting

---
+++ type-casting/if.js
@@ -0,0 +1,8 @@
+const done = true;
+let message = "";
+
+if (done) {
+  message = "완료";
+}
+message = done && "완료";
+console.log(message);
---
+++ type-casting/toString.js
@@ -0,0 +1,8 @@
+let num = 3;
+const strNum1 = String(num);//String
+const strNum2 = num.toString();//toString
+const strNum3 = num + ''; //''
+
+console.log(strNum1);
+console.log(strNum2);
+console.log(strNum3);
